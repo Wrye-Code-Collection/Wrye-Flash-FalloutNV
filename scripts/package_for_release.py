de061d2289928809a7d006adecc5799fd89ea16a
Sharlikran <danielhmpdx16@gmail.com>
2014-08-23 06:52:30 -0700

    game folder not present in fallout version

--- scripts/package_for_release.py
+++ scripts/package_for_release.py
@@ -241,7 +241,8 @@ def CreateStandaloneExe(args, file_version):
     # Determine the extra includes needed (because py2exe wont automatically
     # detect these)
     includes = []
-    for file in os.listdir(os.path.join(mopy, u'bash', u'game')):
+    # for file in os.listdir(os.path.join(mopy, u'bash', u'game')):
+    for file in os.listdir(os.path.join(mopy, u'bash')):
         if file.lower()[-3:] == u'.py':
             if file.lower() != u'__init__.py':
                 includes.append("'bash.game.%s'" % file[:-3])
