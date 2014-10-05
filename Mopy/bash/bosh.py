0b08c36e3481f89634721c1021d85f70211462ff
Sharlikran <danielhmpdx16@gmail.com>
2014-10-04 17:55:18 -0700

    Added MreAlch to Sound Patcher

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -25896,6 +25896,8 @@ class SoundPatcher(ImportPatcher):
             recAttrs_class[recClass] = ('soundLevel','sound1','sound2')
         for recClass in (MreProj,):
             recAttrs_class[recClass] = ('sound','soundCountDown','soundDisable','soundLevel')
+        for recClass in (MreAlch,):
+            recAttrs_class[recClass] = ('soundPickUp','soundDrop','soundConsume'),
         for recClass in (MreAspc,):
             recAttrs_class[recClass] = ('soundLooping','useSoundFromRegion','environmentType')
         for recClass in (MreWeap,):
