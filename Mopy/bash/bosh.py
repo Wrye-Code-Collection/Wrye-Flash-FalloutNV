0b612e5ac2984072415da106909734c677e98be7
Sharlikran <danielhmpdx16@gmail.com>
2015-01-26 12:19:10 -0800

    Commented out def setRace
    NPCs do not have an FNAM subrecord or any equivalent

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -2440,6 +2440,9 @@ class MreArma(MelRecord):
     """Armor addon record."""
     classType = 'ARMA'
     _flags = MelBipedFlags(0L,Flags.getNames())
+    _dnamFlags = Flags(0L,Flags.getNames(
+        (0,'modulatesVoice'),
+    ))
     _generalFlags = Flags(0L,Flags.getNames(
         ( 2,'hasBackpack'),
         ( 3,'medium'),
@@ -2483,7 +2486,7 @@ class MreArma(MelRecord):
         #9:Hand Wear,10:Chems,11:Stimpack,12:Food,13:Alcohol
         MelStruct('ETYP','i',('etype',-1)),
         MelStruct('DATA','IIf','value','health','weight'),
-        MelArmaDnam('DNAM','=hHfI','ar','flags','dt',('armaDnam1',0L)),
+        MelArmaDnam('DNAM','=hHfI','ar',(_dnamFlags,'dnamFlags',0L),'dt',('armaDnam1',0L)),
         )
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
 
@@ -4784,33 +4787,44 @@ class MreNpc(MreActor):
         )
     __slots__ = MreActor.__slots__ + melSet.getSlotsUsed()
 
-    def setRace(self,race):
-        """Set additional race info."""
-        self.race = race
-        #--Model
-        if not self.model:
-            self.model = self.getDefault('model')
-        if race in (0x23fe9,0x223c7):
-            self.model.modPath = r"Characters\_Male\SkeletonBeast.NIF"
-        else:
-            self.model.modPath = r"Characters\_Male\skeleton.nif"
-        #--FNAM
-        # Needs Updating for Fallout New Vegas
-        # American
-        fnams = {
-            0x23fe9 : 0x3cdc ,#--Argonian
-            0x224fc : 0x1d48 ,#--Breton
-            0x191c1 : 0x5472 ,#--Dark Elf
-            0x19204 : 0x21e6 ,#--High Elf
-            0x00907 : 0x358e ,#--Imperial
-            0x22c37 : 0x5b54 ,#--Khajiit
-            0x224fd : 0x03b6 ,#--Nord
-            0x191c0 : 0x0974 ,#--Orc
-            0x00d43 : 0x61a9 ,#--Redguard
-            0x00019 : 0x4477 ,#--Vampire
-            0x223c8 : 0x4a2e ,#--Wood Elf
-            }
-        self.fnam = fnams.get(race,0x358e)
+    # NPCs do not have an FNAM subrecord or any equivalent
+    # def setRace(self,race):
+    #     """Set additional race info."""
+    #     self.race = race
+    #     #--Model
+    #     if not self.model:
+    #         self.model = self.getDefault('model')
+    #     if race in (0x23fe9,0x223c7): # Argonian, Khajiit
+    #         self.model.modPath = r"Characters\_Male\SkeletonBeast.NIF"
+    #     else:
+    #         self.model.modPath = r"Characters\_Male\skeleton.nif"
+    #     #--FNAM
+    #     # Needs Updating for Fallout New Vegas
+    #     # American
+    #     fnams = {
+    #         0x00424a : 0xdf93 ,#--AfricanAmerican
+    #         0x0042be : 0x22fb ,#--AfricanAmerican Child
+    #         0x0042bf : 0x08ab ,#--AfricanAmerican Old
+    #         0x0987de : 0x3dbc ,#--AfricanAmerican Old Aged
+    #         0x04bf72 : 0x854f ,#--AfricanAmerican Raider
+    #         0x0038e6 : 0x8b1b ,#--Asian
+    #         0x0042c0 : 0x7277 ,#--Asian Child
+    #         0x0042c1 : 0x77e5 ,#--Asian Old
+    #         0x0987dd : 0x62ff ,#--Asian Old Aged
+    #         0x04bf71 : 0x83e0 ,#--Asian Raider
+    #         0x000019 : 0x8778 ,#--Caucasian
+    #         0x0042c2 : 0x2e79 ,#--Caucasian Child
+    #         0x0042c3 : 0x2381 ,#--Caucasian Old
+    #         0x0987df : 0x8295 ,#--Caucasian Old Aged
+    #         0x04bb8d : 0x383b ,#--Caucasian Raider
+    #         0x003b3e : 0xdb71 ,#--Ghoul
+    #         0x0038e5 : 0x010c ,#--Hispanic
+    #         0x0042c4 : 0x6c40 ,#--Hispanic Child
+    #         0x0042c5 : 0x99ad ,#--Hispanic Old
+    #         0x0987dc : 0xac2d ,#--Hispanic Old Aged
+    #         0x04bf70 : 0xba0f ,#--Hispanic Raider
+    #         }
+    #     self.fnam = fnams.get(race,0x8778)
 
 #------------------------------------------------------------------------------
 class MrePack(MelRecord):
@@ -20003,7 +20017,8 @@ class PCFaces:
         #--Set face
         npc.full = face.pcName
         npc.flags.female = (face.gender & 0x1)
-        npc.setRace(masterMap(face.race,0x00907)) #--Default to Imperial
+        # Fallout NPCs do not have an FNAM subrecord or any equivalent
+        # npc.setRace(masterMap(face.race,0x00907)) #--Default to Imperial
         npc.eye = masterMap(face.eye,None)
         npc.hair = masterMap(face.hair,None)
         npc.hairLength = face.hairLength
@@ -20235,7 +20250,8 @@ class PCFaces:
         npc.full = face.pcName
         npc.flags.female = face.gender
         npc.iclass = masterMap(face.iclass,0x237a8) #--Default to Acrobat
-        npc.setRace(masterMap(face.race,0x00907)) #--Default to Imperial
+        # Fallout NPCs do not have an FNAM subrecord or any equivalent
+        # npc.setRace(masterMap(face.race,0x00907)) #--Default to Imperial
         npc.eye = masterMap(face.eye,None)
         npc.hair = masterMap(face.hair,None)
         npc.hairLength = face.hairLength
